86e105d0d392c8262b81728e7be7006c1f67b0f1
bezet <bartosz.zadara@gmail.com>
2018-02-09 14:55:23 +0100

    Tidy up

--- generators/app/index.js
+++ generators/app/index.js
@@ -68,16 +68,6 @@ module.exports = class extends Generator {
   writing() {
     this._writingLibraryFiles();
     this._writingDemoFiles();
-//     this._writingScripts();
-//     this._writingStyles();
-//     this._writingTemplates();
-//     this._writingPackageJSON();
-//     this._writingGulpfile();
-//     this._writingBabel();
-//     this._writingGit();
-//     this._writingWebpackConfig();
-//     this._writingModernizrConfig();
-//     this._writingReadme();
   }
 
   _writingLibraryFiles() {
@@ -140,79 +130,6 @@ module.exports = class extends Generator {
       }
     );
   }
-
-//   _writingScripts() {
-//     this.fs.copy(
-//       this.templatePath('src/scripts/index.js'),
-//       this.destinationPath('src/scripts/index.js')
-//     );
-//   }
-
-//   _writingStyles() {
-//     this.fs.copy(
-//       this.templatePath('src/scss/main.scss'),
-//       this.destinationPath('src/scss/main.scss')
-//     );
-//   }
-
-//   _writingTemplates() {
-//     this.fs.copy(
-//       this.templatePath('src/templates/index.html'),
-//       this.destinationPath('src/templates/index.html')
-//     );
-//   }
-
-//   _writingGulpfile() {
-//     this.fs.copy(
-//       this.templatePath('gulpfile.babel.js'),
-//       this.destinationPath('gulpfile.babel.js')
-//     );
-//   }
-
-//   _writingPackageJSON() {
-//     this.fs.copy(
-//       this.templatePath('_package.json'),
-//       this.destinationPath('package.json')
-//     );
-//   }
-
-//   _writingBabel() {
-//     this.fs.copy(
-//       this.templatePath('babelrc'),
-//       this.destinationPath('.babelrc')
-//     );
-//   }
-
-//   _writingGit() {
-//     this.fs.copy(
-//       this.templatePath('gitignore'),
-//       this.destinationPath('.gitignore'));
-
-//     this.fs.copy(
-//       this.templatePath('gitattributes'),
-//       this.destinationPath('.gitattributes'));
-//   }
-
-//   _writingWebpackConfig() {
-//     this.fs.copy(
-//       this.templatePath('webpack.config.js'),
-//       this.destinationPath('webpack.config.js')
-//     );
-//   }
-
-//   _writingModernizrConfig() {
-//     this.fs.copy(
-//       this.templatePath('modernizr-config.json'),
-//       this.destinationPath('modernizr-config.json')
-//     );
-//   }
-
-//   _writingReadme() {
-//     this.fs.copy(
-//       this.templatePath('README.md'),
-//       this.destinationPath('README.md')
-//     );
-//   }
 
   install() {
     this.installDependencies({
